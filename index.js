a617a2d1975f4263db3828bd93bb54492a6b908a
Michael Lobman <michael.lobman@gmail.com>
2021-11-05 09:37:50 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution here
+/*
+function findMatching(array, string) {
+	array.filter(looseMatch(name));
+}
+
+
+function looseMatch(name) {
+	return array.name.toLowerCase() === string.toLowerCase();
+}
+*/
+
+function findMatching(array, string) {
+	return array.filter (element => element.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch(array, string) {
+	return array.filter (element => element.startsWith(string));
+}
+
+
+function matchName(array, string) {
+	return array.filter(function(element){
+		return element.name === string;
+	})
+}
